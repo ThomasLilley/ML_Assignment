a91344284df91f8663041529d8de545cda3759f4
ThomasLilley <thomas.lilley01@gmail.com>
2019-04-24 21:10:34 +0100

    task one complete, sanitised strings for task 2

--- Task_1.py
+++ Task_1.py
@@ -1,24 +1,19 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-# from sklearn.gaussian_process import GaussianProcessClassifier
-# from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
-# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.neural_network import MLPClassifier
 from matplotlib.image import imread
-# from sklearn.metrics import accuracy_score
-# import matplotlib as plt
 import numpy as np
 import glob
 import pickle
 import sys
 
-names = ["Nearest Neighbor", "Linear SVM", "Gaussian Process",
-         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes"]
+names = ["Nearest Neighbor", "SVC Linear SVM", "SVC Gaussian Process",
+         "Decision Tree", "Random Forest", "MLP Classifier", "AdaBoost",
+         "Naive Bayes (GaussianNB)"]
 
 classifiers = [
     KNeighborsClassifier(3),
@@ -35,22 +30,19 @@ def task1():
     print('\nTASK 1')
     print('------')
     tr_dir = 'digits-train-5000'
-    te_dir = 'digits-test'
     v_dir = 'digits-validation-1000'
     f_ext = '/*.png'
 
     t_images, t_labels = load_dataset(tr_dir, f_ext)
-    te_images, te_labels = load_dataset(te_dir, f_ext)
     v_images, v_labels = load_dataset(v_dir, f_ext)
     t_images = reshape(t_images)
-    te_images = reshape(te_images)
     v_images = reshape(v_images)
     flag = True
     while flag:
         print("--------------")
-        a = input("Begin Training? y/n :")
+        a = input("Begin Training? y/n : ")
         if a == 'y' or a == 'Y':
-            train(t_images, t_labels, v_images, v_labels, te_images, te_labels)
+            train(t_images, t_labels)
             flag = False
         elif a == 'n' or a == 'N':
             flag = False
@@ -60,21 +52,14 @@ def task1():
     flag = True
     while flag:
         print("--------------")
-        a = input("Begin Prediction? y/n :")
+        a = input("Begin Prediction? y/n : ")
         if a == 'y' or a == 'Y':
-            predict(t_images, t_labels, v_images, v_labels, te_images, te_labels)
+            predict(v_images, v_labels)
             flag = False
         elif a == 'n' or a == 'N':
             flag = False
         else:
             print("Invalid input \n")
-
-
-    # flag = True
-    # while flag:
-    #     a = input("Predict (E)")
-    #     if a == 'E':
-    #         flag = False
 
 
 def load_dataset(set_n, ext):
@@ -102,7 +87,7 @@ def reshape(arr):
     return d2_arr
 
 
-def train(t_images, t_labels, v_images, v_labels, te_images, te_labels):
+def train(t_images, t_labels):
 
     print("\n")
 
@@ -123,53 +108,49 @@ def train(t_images, t_labels, v_images, v_labels, te_images, te_labels):
         i += 1
 
 
-def predict(t_images, t_labels, v_images, v_labels, te_images, te_labels):
+def predict(v_images, v_labels):
     ext = ".pkl"
     flag = True
     while flag:
-        print("Prediction Main Menu")
-
+        print("\nPrediction Main Menu")
         print("1) Nearest Neighbor")
         print("2) Linear SVM")
         print("3) Gaussian Process")
         print("4) Decision Tree")
         print("5) Random Forest")
-        print("6) Neural Net")
+        print("6) MLP")
         print("7) AdaBoost")
         print("8) Naive Bayes")
-        a = input("Which Classifier would you like to use?")
-        a = int(a)
-        if a in range(1, 8):
-            print("prediction using: ", names[a-1])
-            # Load from file
-            filename = names[a] + ext
-            with open(filename, 'rb') as file:
-                pickle_model = pickle.load(file)
+        print("-")
+        print("9) All of the above")
+        print("-")
+        print("0) Return to Main Menu")
+        a = input("Which Classifier would you like to use? : ")
+        if a.isdigit():
+            a = int(a)
+            if a in range(1, 8):
+                print("prediction using: ", names[a-1])
+                # Load from file
+                filename = names[a-1] + ext
+                with open(filename, 'rb') as file:
+                    pickle_model = pickle.load(file)
 
-            # Calculate the accuracy score and predict target values
-            score = pickle_model.score(te_images, te_labels)
-            print("Test score: ", (score*100))
-            out = pickle_model.predict(te_images)
+                # Calculate the accuracy score and predict target values
+                score = pickle_model.score(v_images, v_labels)
+                print("Test score: ", (score*100), "%")
 
-            for i in range(0, len(out)):
-                print(out[i], te_labels[i])
+            elif a == 9:
+                for i in range(0, len(names)):
+                    print("\nprediction using: ", str((i+1))+")", names[i])
+                    # Load from file
+                    filename = names[i] + ext
+                    with open(filename, 'rb') as file:
+                        pickle_model = pickle.load(file)
+                    score = pickle_model.score(v_images, v_labels)
+                    print("Test score: ", (score * 100), "%")
+            elif a == 0:
+                flag = False
         else:
             print("invalid input")
-
-
-    # print("Training K-nearest neighbour classifier...")
-    # knn_classifier = KNeighborsClassifier(3)
-    # knn_classifier.fit(t_images, t_labels)
-    # accuracy = knn_classifier.score(v_images, v_labels)
-    # print("\nKKN Accuracy = " + str(accuracy * 100) + "%")
-    # print("\nKNN Test Images Prediction = ", knn_classifier.predict(te_images), "Ground Truth = ", te_labels)
-    #
-    # print("Training random forest classifier:")
-    # r_forest_classifier = RandomForestClassifier(3)
-    # r_forest_classifier.fit(t_images, t_labels)
-    # accuracy = r_forest_classifier.score(v_images, v_labels)
-    # print("\nRandom Forest Accuracy = " + str(accuracy * 100) + "%\n")
-    # print("Random Forest Test Images Prediction = ", r_forest_classifier.predict(te_images),
-    #       "Ground Truth = ", te_labels)
 
 
